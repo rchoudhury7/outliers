ae799dcbd62de27220782cc29e193cde33299f83
Fabian Gieseke <fgieseke@cs.ru.nl>
2016-04-29 10:51:39 +0200

    automatic data download

--- data/_crtsimg.py
+++ data/_crtsimg.py
@@ -1,12 +1,24 @@
 import os
+import tarfile
 from astropy.io import fits
 from _generate_util import *
 
 def get_crtsimg(fname):
-    
-    imgpath = os.path.join(get_data_path(), "CRTS_IMAGES", "CSS_trans_ims", fname)
 
-    hdu_list = fits.open(imgpath)
+    p = os.path.join(get_data_path(), "CRTS_IMAGES")
+    imgpath = os.path.join(p, "CSS_trans_ims")
+
+    if not os.path.isdir(imgpath):
+
+        fnamezip = os.path.join(p, "CSS_trans_ims.tar.gz")
+        check_and_download(fnamezip)
+
+        print("Extracting content ...")
+        tar = tarfile.open(fnamezip, "r:gz")
+        tar.extractall(path=imgpath)
+        tar.close()
+
+    hdu_list = fits.open(os.path.join(imgpath, fname))
     image_data = hdu_list[0].data
     hdu_list.close()
     return image_data
